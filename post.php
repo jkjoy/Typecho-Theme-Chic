5932a9ea2c5a26eeb3ec9f6386cc8376a49aebe3
浪子 <jkjoy@live.cn>
2024-07-09 18:54:56 +0800

    1.0.2

--- post.php
+++ post.php
@@ -43,10 +43,12 @@
                 <span class="tag">
                 <?php if ($this->tags): ?>
                   <?php foreach ($this->tags as $tag): ?>
-                 <a href="<?php echo $tag['permalink']; ?>">#<?php echo $tag['name']; ?></a>
+                 <a href="<?php echo $tag['permalink']; ?>">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="14" height="14" fill="rgba(87,107,79,1)"><path d="M7.78428 14L8.2047 10H4V8H8.41491L8.94043 3H10.9514L10.4259 8H14.4149L14.9404 3H16.9514L16.4259 8H20V10H16.2157L15.7953 14H20V16H15.5851L15.0596 21H13.0486L13.5741 16H9.58509L9.05957 21H7.04855L7.57407 16H4V14H7.78428ZM9.7953 14H13.7843L14.2047 10H10.2157L9.7953 14Z"></path></svg>
+                    <?php echo $tag['name']; ?></a>
                      <?php endforeach; ?>
                  <?php else: ?>
-                 无标签
+              
                 <?php endif; ?>
                 </span>
             </div>
